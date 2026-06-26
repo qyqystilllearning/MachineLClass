4761e9622f337a7d8fcd0b1b5686286f0e1a33c0
qyqystilllearning <capt.rizqy@gmail.com>
2026-06-27 05:40:34 +0800

    update

--- Exercise/26000108_MuhammadRizqyHidayat_Exercise.r
+++ Exercise/26000108_MuhammadRizqyHidayat_Exercise.r
@@ -1,3 +1,6 @@
+# Muhammad Rizqy Hidayat - Exercise Data Science
+# 26000108
+
 # 1.Creating DataFrame
 
 # Create vectors for each column
@@ -18,12 +21,53 @@ View(students)
 # 2. Access and Subset Data
 
 #Display the Test_Score column using both df['Test_Score'] and df$Test_score.
-
 students["Test_Score"]
 
 students$Test_Score
-#Display the first 3 rows of the data frame
 
-students[]
+#Display the first 3 rows of the data frame
+students[1:3, ]
+
 #Display students who are older than 22
+students[students$Age > 22, ]
+
 #Display female students with Test_Score greater than 80
+students[students$Gender == "Female" & students$Test_Score > 80, ]
+
+# 3. Add New Data
+
+new_student <- data.frame(
+  Name = "Lina",
+  Gender = "Male",
+  Age = 20,
+  Test_Score = 95,
+  Attendance = 92
+)
+
+students <- rbind(students, new_student)
+
+print(students)
+
+# 4
+
+students_na <- data.frame(
+  Name = c("Aisha", "Bryan", "Chloe", "Daniel", "Emily"),
+  Gender = c("Female", "Male", "Female", "Male", "Female"),
+  Age = c(22, NA, 21, 24, 22),
+  Test_Score = c(85, 78, NA, 65, 88),
+  Attendance = c(90, 85, 95, NA, 88)
+)
+
+#Check for missing values (NA) in the data frame
+is.na(students_na)
+
+#Replace missing values in Age with the mean age
+students_na$Age[is.na(students_na$Age)] <- mean(students_na$Age, na.rm = TRUE)
+
+#Replace missing values in Test_Score with the median test score
+students_na$Test_Score[is.na(students_na$Test_Score)] <- median(students_na$Test_Score, na.rm = TRUE)
+
+#Replace missing values in Attendance with 0
+students_na$Attendance[is.na(students_na$Attendance)] <- 0
+
+print(students_na)
